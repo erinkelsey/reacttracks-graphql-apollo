e7ae058008a0376b2b5f848d166fd6eca4f3591b
erinszilagyi <erinszilagyi@gmail.com>
2020-07-09 22:10:21 -0500

    implemented app bar, loading, and signout functionality

--- src/Root.js
+++ src/Root.js
@@ -6,18 +6,21 @@ import withRoot from "./withRoot";
 import App from "./pages/App";
 import Profile from "./pages/Profile";
 import Header from "./components/Shared/Header"
+import Loading from "./components/Shared/Loading";
+import Error from "./components/Shared/Error";
 
 const Root = () => (
   <Query query={ME_QUERY}>
     {/* render prop function */}
     {({data, loading, error}) => {
-      if (loading) return <div>Loading</div>
-      if (error) return <div>Error</div>
+      if (loading) return <Loading />;
+      if (error) return <Error error={error}/>;
+      const currentUser = data.me;
 
       return (
         <Router>
           <>
-            <Header />
+            <Header currentUser={currentUser} />
             <Switch>
               <Route exact path="/" component={App} />
               <Route path="/profile/:id" component={Profile} />

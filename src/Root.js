b7102a523aa938d2467cc8a5aac0f37283dd8fa8
erinszilagyi <erinszilagyi@gmail.com>
2020-07-09 21:38:45 -0500

    implement register and login

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,54 @@
 import React from "react";
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import withRoot from "./withRoot";
+import App from "./pages/App";
+import Profile from "./pages/Profile";
+import Header from "./components/Shared/Header"
 
-const Root = () => <div>Root</div>;
+const Root = () => (
+  <Query query={ME_QUERY}>
+    {/* render prop function */}
+    {({data, loading, error}) => {
+      if (loading) return <div>Loading</div>
+      if (error) return <div>Error</div>
+
+      return (
+        <Router>
+          <>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/profile/:id" component={Profile} />
+            </Switch>
+          </>
+        </Router>
+      )
+    }}
+  </Query>
+)
+
+const ME_QUERY = gql`
+  {
+    me {
+      id
+      username
+      email
+    }
+  }
+
+`
+
+// const GET_TRACKS_QUERY = gql`
+//   {
+//     tracks {
+//       id
+//       title
+//       description
+//       url
+//     }
+//   }
+// `
 
 export default withRoot(Root);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Root from "./Root";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+import { ApolloProvider, Query } from 'react-apollo';
+import ApolloClient, { gql } from 'apollo-boost';
+
+import Root from "./Root";
+import Auth from './components/Auth';
+
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:8000/graphql/',
+  // include auth header on requests
+  fetchOptions: {
+    credentials: "include"
+  },
+  request: operation => {
+    const token = localStorage.getItem('authToken') || "";
+    operation.setContext({
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    })
+  },
+  clientState: {
+    defaults: {
+      // !! convert string or any value to boolean
+      isLoggedIn: !!localStorage.getItem('authToken')
+    }
+  }
+})
+
+const IS_LOGGED_IN_QUERY = gql`
+  query {
+    isLoggedIn @client
+  }
+`
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Query query={IS_LOGGED_IN_QUERY}>
+      {({ data }) => (data.isLoggedIn ? <Root /> : <Auth />) }
+    </Query>
+  </ApolloProvider>, 
+  document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
